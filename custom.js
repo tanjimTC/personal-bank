c0073bc219605cd2573ef3d55239d4a7e0546a12
tanjimTC <tanjimtc74@gmail.com>
2020-03-26 04:21:58 +0600

    fields updated

--- custom.js
+++ custom.js
@@ -1,29 +1,46 @@
-document.getElementById("login").addEventListener("click",()=>{
-    document.getElementById("login-area").style.display="none";
-    document.getElementById("transaction").style.display = "block";
+document.getElementById("login").addEventListener("click", () => {
+  document.getElementById("login-area").style.display = "none";
+  document.getElementById("transaction").style.display = "block";
 });
 
-document.getElementById("deposit").addEventListener("click",()=>{
-    var depositAmount=document.getElementById("depositAmount").value;
-    let depositNumber = parseFloat(depositAmount);
 
-
-    // let currentDepositAmount =document.getElementById("currentdepositNumber").innerText;
-    // let finalDepositAmount2 = parseFloat(currentDepositAmount);
-    // let finalDepositAmount = depositNumber + finalDepositAmount2;
-    // document.getElementById("currentdepositNumber").innerText =finalDepositAmount;
-
-
-    updateField("currentdepositNumber",depositNumber);
-
-    document.getElementById("depositAmount").value ="";
-
-    updateField("balance",depositNumber);
-    
+//deposit button handler
+document.getElementById("deposit").addEventListener("click", () => {
+  let depositNumber = getAmount("depositAmount");
+  updateField("currentdepositNumber", depositNumber);
+  document.getElementById("depositAmount").value = "";
+  updateField("balance", depositNumber);
 });
- function updateField (id, lastNumber){
+
+
+
+//withdraw button handler
+document.getElementById("withdraw").addEventListener("click", () => {
+  let withdrawNumber = getAmount("WithdrawAmount");
+  updateField("currentwithdraw", withdrawNumber);
+  document.getElementById("WithdrawAmount").value = "";
+  updateField2("balance",withdrawNumber);
+});
+
+
+function getAmount(id) {
+  let amount = document.getElementById(id).value;
+  let amountNumber = parseFloat(amount);
+  return amountNumber;
+}
+
+//deposit adding
+function updateField(id, lastNumber) {
+  let balance = document.getElementById(id).innerText;
+  let balanceFloat = parseFloat(balance);
+  let newBalance = balanceFloat + lastNumber;
+  document.getElementById(id).innerText = newBalance;
+}
+
+//withdraw subtracting
+function updateField2(id, lastNumber) {
     let balance = document.getElementById(id).innerText;
     let balanceFloat = parseFloat(balance);
-    let newBalance = balanceFloat+lastNumber;
+    let newBalance = balanceFloat - lastNumber;
     document.getElementById(id).innerText = newBalance;
- }
+}
