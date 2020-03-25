9f53d6d59b36a86fb933e934392e3744d7c487af
tanjimTC <tanjimtc74@gmail.com>
2020-03-26 02:59:20 +0600

    2 field updated

--- custom.js
+++ custom.js
@@ -2,3 +2,28 @@ document.getElementById("login").addEventListener("click",()=>{
     document.getElementById("login-area").style.display="none";
     document.getElementById("transaction").style.display = "block";
 });
+
+document.getElementById("deposit").addEventListener("click",()=>{
+    var depositAmount=document.getElementById("depositAmount").value;
+    let depositNumber = parseFloat(depositAmount);
+
+
+    // let currentDepositAmount =document.getElementById("currentdepositNumber").innerText;
+    // let finalDepositAmount2 = parseFloat(currentDepositAmount);
+    // let finalDepositAmount = depositNumber + finalDepositAmount2;
+    // document.getElementById("currentdepositNumber").innerText =finalDepositAmount;
+
+
+    updateField("currentdepositNumber",depositNumber);
+
+    document.getElementById("depositAmount").value ="";
+
+    updateField("balance",depositNumber);
+    
+});
+ function updateField (id, lastNumber){
+    let balance = document.getElementById(id).innerText;
+    let balanceFloat = parseFloat(balance);
+    let newBalance = balanceFloat+lastNumber;
+    document.getElementById(id).innerText = newBalance;
+ }
