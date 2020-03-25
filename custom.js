ddaec378b8eb7e00c7b80623c0f381c1686350bb
tanjimTC <tanjimtc74@gmail.com>
2020-03-26 00:25:08 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,4 @@
+document.getElementById("login").addEventListener("click",()=>{
+    document.getElementById("login-area").style.display="none";
+    document.getElementById("transaction").style.display = "block";
+});
